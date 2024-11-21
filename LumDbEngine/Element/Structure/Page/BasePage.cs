e7cd86e693ec6d16f36e86d5633a84ea6369708a
RoJ <51768459+LdotJdot@users.noreply.github.com>
2024-11-21 16:21:37 +0800

    optimize the write block size to 4096 bytes. (#10)

--- LumDbEngine/Element/Structure/Page/BasePage.cs
+++ LumDbEngine/Element/Structure/Page/BasePage.cs
@@ -78,12 +78,7 @@ namespace LumDbEngine.Element.Structure.Page
         public abstract void Read(BinaryReader br);
 
         protected void BasePageWrite(BinaryWriter bw)
-        {
-            var pos = DbHeader.HEADER_SIZE + (long)PageId * PAGE_SIZE;
-            var endPos = pos + PAGE_SIZE;
-            if (bw.BaseStream.Length < endPos) bw.BaseStream.SetLength(endPos);
-
-            bw.BaseStream.Seek(pos, SeekOrigin.Begin);
+        {           
             bw.Write((byte)Type);
             bw.Write(PageId);
             bw.Write(NextPageId);
@@ -93,6 +88,15 @@ namespace LumDbEngine.Element.Structure.Page
         protected void MoveToPageHeaderSizeOffset(Stream stream, int headerSize)
         {
             var pos = DbHeader.HEADER_SIZE + (long)PageId * PAGE_SIZE + headerSize;
+            stream.Seek(pos, SeekOrigin.Begin);
+        }
+             
+        protected void MoveToPageStart(Stream stream)
+        {
+            var pos = DbHeader.HEADER_SIZE + (long)PageId * PAGE_SIZE;
+            var endPos = pos + PAGE_SIZE;
+            if (stream.Length < endPos) stream.SetLength(endPos);
+
             stream.Seek(pos, SeekOrigin.Begin);
         }
 
